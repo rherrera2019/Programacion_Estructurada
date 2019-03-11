3347f7f8fec52ce058e5cf26f675f4b31fc80859
HikingCarrot7 <47760015+HikingCarrot7@users.noreply.github.com>
2019-03-10 18:47:27 -0600

    Create 13.c

---
+++ Parcial I/13.c
@@ -0,0 +1,22 @@
+/*Imprime dos líneas distintas con las frases: 1) "Bienvenido a la programación en 
+C" y 2) "Comenzamos a programar".
+Fecha: 17/ene/2019
+Version: 1.0 */
+
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+	
+	//Mostrar línea por pantalla
+	printf ("\nBienvenido a la programación en C");
+	
+	//Mostrar segunda línea por pantalla
+	printf ("\n\nComenzamos a programar");
+	
+	//Esperar un caracter del teclado
+	getch();
+	
+	return 0;
+}
+
