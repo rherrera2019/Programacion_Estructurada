3f8757df36a34c6c5244c5d52e3356dc2d62f492
MCAlberto55 <47836982+MCAlberto55@users.noreply.github.com>
2019-03-15 19:15:26 -0600

    Add files via upload

---
+++ Parcial I/Punto C/Ejercicio45(Macros).c
@@ -0,0 +1,74 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <conio.h> 
+
+/*Ejercicio 45: cálculo de sueldo según años 
+Descripción: E.P. que lea el nombre, la antigüedad y sueldo de cada uno de los 100
+empleados y que calcule e imprima la cantidad que le corresponde a
+cada uno
+Fecha: 15/02/2019
+Versión: 1.0 */
+
+//declaracion de macros
+#define para(x) for (i=1;1<=100;i++) //macro para el para
+//macros para los clases planteados en el problema
+#define caso1 sueldo=sueldo*1.01; 
+#define caso2 sueldo=sueldo*1.03;
+#define caso3 sueldo=sueldo*1.05;
+#define caso4 sueldo=sueldo*1.07;
+
+int main () 
+{
+	//Declaracion de variables 
+	float sueldo;
+	int tiempo;
+	char nombre[20];
+	int i; 
+	
+	//entradas
+	para(x) {
+		
+		printf ("\n Dime el nombre del trabajador numero %d : ",i );
+		scanf ("%s",&nombre);
+		fflush (stdin) ;
+		
+		printf ("\n Dime el sueldo del trabajador : " ); 
+		scanf 	("%f",&sueldo); 
+		fflush (stdin) ;
+		
+		printf ("\n Dime el tiempo en años que ha trabajado aqui : " );
+		scanf  ("%d",&tiempo); 
+		fflush (stdin) ;
+		
+		//proceso
+		
+		if (sueldo>=1&&tiempo<=3){
+			caso1
+		}
+		else
+			if (sueldo>=4&&tiempo<=6){
+				caso2
+		}
+			else
+				if (sueldo>=7&&tiempo<=9){
+					caso3
+			}
+				else
+					if (sueldo>=10&&tiempo<=15){
+						caso4
+				}
+					else 
+						printf("\n no amerita incentivo" );
+					
+					//salida
+					printf ("\n El sueldo de %s es de %.2f", nombre,sueldo );
+					
+					getch (); 
+					
+					system("cls");
+	}
+	
+	getch () ; 
+	
+	return 0;
+}
---
+++ Parcial I/Punto C/Ejercicio45.c
@@ -0,0 +1,67 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <conio.h> 
+
+/*Ejercicio 45: cálculo de sueldo según años 
+Descripción: E.P. que lea el nombre, la antigüedad y sueldo de cada uno de los 100
+empleados y que calcule e imprima la cantidad que le corresponde a
+cada uno
+Fecha: 15/02/2019
+Versión: 1.0 */
+ int main () 
+ {
+	 //Declaracion de variables 
+	float sueldo;
+	int tiempo;
+	char nombre[20];
+	int i; 
+	
+	//entradas
+	for (i=1;1<=100;i++){
+		
+		printf ("\n Dime el nombre del trabajador numero %d : ",i );
+		scanf ("%s",&nombre);
+		fflush (stdin) ;
+			
+		printf ("\n Dime el sueldo del trabajador : " ); 
+		scanf 	("%f",&sueldo); 
+		fflush (stdin) ;
+			
+		printf ("\n Dime el tiempo en años que ha trabajado aqui : " );
+		scanf  ("%d",&tiempo); 
+		fflush (stdin) ;
+		
+		//proceso
+		
+		if (sueldo>=1&&tiempo<=3){
+			sueldo=sueldo*1.01;
+		}
+		else
+			if (sueldo>=4&&tiempo<=6){
+				sueldo=sueldo*1.03;
+		}
+			else
+					if (sueldo>=7&&tiempo<=9){
+						sueldo=sueldo*1.05;
+			}
+					else
+							
+							if (sueldo>=10&&tiempo<=15){
+								sueldo=sueldo*1.07;
+					}
+							else 
+								
+								printf("\n no amerita incentivo" );
+		
+		//salida
+		printf ("\n El sueldo de %s es de %.2f", nombre,sueldo );
+		
+		getch (); 
+		
+		system("cls");
+	}
+	
+	getch () ; 
+	
+	return 0;
+ }
