30bdce5c3b498d6fcb026e2c8ef43a41a4d8a0dd
MCAlberto55 <47836982+MCAlberto55@users.noreply.github.com>
2019-04-03 10:34:10 -0600

    Add files via upload

---
+++ Parcial II/Punto_C/problema 70.c
@@ -0,0 +1,63 @@
+#include <stdio.h> 
+#include <string.h>
+#include <stdlib.h>
+
+
+/*Problema 70. Punto C
+Descripción: 70. E.P. que reserve memoria para un arreglo de 10 elementos
+ de tipo float, rellenarlo con datos que digite el usuario, luego utilizar
+ realloc para ampliar dicho arreglo a 20 elementos e imprimir el arreglo
+ resultante.
+Fecha: 31/03/2019
+Versión: 1.0 */
+
+int main ()
+{
+//Declaración de variables 
+	float *vector;
+	int i;
+	float elemento;
+	
+	vector =malloc(10*sizeof(float)); 
+	/*Reservo memoria para 10 elementos e 
+	indico que son datos de tipo float*/
+	
+//Lectura de datos 
+	for (i=0;i<10;i++)
+	{
+		printf("\n Deme el elemento %d del vector : ",i+1);
+		scanf("%f",&elemento);
+		vector[i]=elemento;
+	}
+	printf("\n -------------------------------------");
+	printf("\n Vector de 10 elementos");
+	
+	for (i=0;i<10;i++)
+	{
+		printf("\n %.2f",vector[i]);
+	}
+	
+//Proceso 
+	printf("\n --------------------------------------------");
+	printf("\n Ejecutando realloc...");
+	printf("\n Arreglo ampliado a 20 elementos exitosamente");
+	realloc(vector,20*sizeof(float)); //vector pasa de 10 a 20 elementos
+
+	for (i=10;i<20;i++)
+	{
+		printf("\n Deme el elemento %d del vector : ",i+1);
+		scanf("%f",&elemento);
+		vector[i]=elemento;
+	}
+//Salida
+	printf("\n -------------------------------------");
+	printf("\n Vector resultante");
+	for (i=0;i<20;i++)
+	{
+		printf("\n %.2f",vector[i]);
+	}
+
+ 
+	return 0;
+
+}
---
+++ Parcial II/Punto_C/problema 71.c
@@ -0,0 +1,56 @@
+#include <stdio.h> 
+#include <string.h>
+#include <stdlib.h>
+
+/*Problema 71. Punto C 
+Descripción: 71. E.P. que utilice un apuntador de arreglo con datos
+ de tipo float, pedir el usuario el número de elementos, luego reservar
+ memoria dinámica con calloc, leer dicho arreglo y posteriormente, 
+imprimir todos sus elementos; por ultimo, liberar el espacio de
+memoria dinámica utilizado
+Fecha: 31/03/2019
+Versión: 1.0 */
+
+int main ()
+{
+//Declaración de variables 
+	float *vector;
+	int i,elementos;
+	float elemento;
+	
+//Lectura de datos 
+	printf("\n Deme el numero de elementos : ");
+	scanf("%d",&elementos);
+	
+//Proceso
+	printf("\n ---------------------------------------");
+	printf("\n Lectura de datos");
+	printf("\n");
+	
+	vector=calloc(elementos,sizeof(float)); //reservando memoría
+	
+	for (i=0;i<elementos;i++) //pidiendo elementos del vector 
+	{
+		printf("\n Digite el elemento %d del vector : ",i+1);
+		scanf("%f",&elemento);
+		
+		vector[i]=elemento;
+	}
+	
+//Salida
+	printf("\n ----------------------------------------");
+	printf("\n Arreglo resultante ");
+	for (i=0;i<elementos;i++)
+	{
+		printf("\n %.2f ",vector[i]);
+	}
+	
+	free(vector); //liberando memoria
+	printf("\n ----------------------------------------");
+	printf("\n Comprobando liberacion de espacio ");//imprimiendo vector vacio
+	for (i=0;i<elementos;i++)
+	{
+		printf("\n %.2f ",vector[i]);
+	}
+	return 0;
+}
