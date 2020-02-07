be8e5d7b27ecbd7eb5306a13ce9b771408a54f2c
GuillermoCanto <56968560+GuillermoCanto@users.noreply.github.com>
2020-02-06 23:03:08 -0600

    Ejercicio36

---
+++ BlueCode/ADA4/Ejercicio36/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+	Name: Ejercicio 36
+	Copyright: BlueCode
+	Author: Guillermo Canto
+	Date: 06/02/20 21:58
+	Description: E.P.  que  lea  50  números  y que  cuente  e  imprima  cuantos  son negativos.
+*/
+
+
+int main(int argc, char *argv[]) {
+	int contadorNegativos = 0;
+	int i = 0;
+	
+	while(i<50){
+		printf("Ingrese un numero")
+	}
+	return 0;
+}
